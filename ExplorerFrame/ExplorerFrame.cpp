7468fb312df63109a544e0e1cebfb2b569d93151
Dongle <programcake@gmail.com>
2022-11-11 23:42:41 +0700

    Architecture and code prettifying.

--- ExplorerFrame/ExplorerFrame.cpp
+++ ExplorerFrame/ExplorerFrame.cpp
@@ -6,33 +6,33 @@
 #include "..\Injector\WndProcExport.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    hUxtheme = LoadLibraryExW(L"uxtheme.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+	hUxtheme = LoadLibraryExW(L"uxtheme.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
 
-    MyRegisterClass(hInstance);
+	MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    MSG msg;
+	MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 //
@@ -42,24 +42,24 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_EXPLORERFRAME));
-    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = L"ExplorerFrame";
-    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_EXPLORERFRAME));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"ExplorerFrame";
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    auto a = RegisterClassExW(&wcex);
-    return a;
+	auto a = RegisterClassExW(&wcex);
+	return a;
 }
 
 //
@@ -74,51 +74,51 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   static const fnSetPreferredAppMode SetPreferredAppMode = (fnSetPreferredAppMode)GetProcAddress(hUxtheme, MAKEINTRESOURCEA(135));
-   SetPreferredAppMode(ForceDark);
+	static const fnSetPreferredAppMode SetPreferredAppMode = (fnSetPreferredAppMode)GetProcAddress(hUxtheme, MAKEINTRESOURCEA(135));
+	SetPreferredAppMode(ForceDark);
 
-   HWND hWnd = CreateWindowW(L"ExplorerFrame", L"", 0,
-      0, 0, 0, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(L"ExplorerFrame", L"", 0,
+		0, 0, 0, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   // Create a new win event hook for every window creation.
-   HWINEVENTHOOK hook = SetWinEventHook(
-       EVENT_OBJECT_CREATE,
-       EVENT_OBJECT_CREATE,
-       hInstance,
-       WinEventProc,
-       0,
-       0,
-       WINEVENT_OUTOFCONTEXT);
+	// Create a new win event hook for every window creation.
+	HWINEVENTHOOK hook = SetWinEventHook(
+		EVENT_OBJECT_CREATE,
+		EVENT_OBJECT_CREATE,
+		hInstance,
+		WinEventProc,
+		0,
+		0,
+		WINEVENT_OUTOFCONTEXT);
 
-    // Enumerate opened windows.
-   EnumWindows([](HWND hwnd, LPARAM lParam) -> BOOL {
-      HandleWindow(hwnd);
-      return TRUE;
-   }, NULL);
+	// Enumerate opened windows.
+	EnumWindows([](HWND hwnd, LPARAM lParam) -> BOOL {
+		HandleWindow(hwnd);
+	return TRUE;
+		}, NULL);
 
-   // Create a notification icon, using version 4
-   // with Windows' built it tooltip and context menu support.
-   NOTIFYICONDATA nid = { sizeof(NOTIFYICONDATA) };
-   wcscpy_s(nid.szTip, L"ExplorerFrame");
-   nid.hWnd = hWnd;
-   nid.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_EXPLORERFRAME));
-   // nid.guidItem = notifyIconGuid;
-   nid.uID = 1;
-   nid.uCallbackMessage = WM_USER + 1;
-   nid.uVersion = NOTIFYICON_VERSION_4;
-   nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP /*| NIF_GUID*/;
+	// Create a notification icon, using version 4
+	// with Windows' built it tooltip and context menu support.
+	NOTIFYICONDATA nid = { sizeof(NOTIFYICONDATA) };
+	wcscpy_s(nid.szTip, L"ExplorerFrame");
+	nid.hWnd = hWnd;
+	nid.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_EXPLORERFRAME));
+	// nid.guidItem = notifyIconGuid;
+	nid.uID = 1;
+	nid.uCallbackMessage = WM_USER + 1;
+	nid.uVersion = NOTIFYICON_VERSION_4;
+	nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP /*| NIF_GUID*/;
 
-   Shell_NotifyIcon(NIM_ADD, &nid);
-   Shell_NotifyIcon(NIM_SETVERSION, &nid);
+	Shell_NotifyIcon(NIM_ADD, &nid);
+	Shell_NotifyIcon(NIM_SETVERSION, &nid);
 
-   return TRUE;
+	return TRUE;
 }
 
 //
@@ -133,20 +133,20 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_DESTROY:
-        {
-        for (const auto& [ _, value ] : hookingMap) {
-            // Try to unhook DLL (wil::unique_hhook::get()), do nothing if failed.
-            try {
-                UnhookWindowsHookEx(value);
-            }
-            catch (...) {}
-        }
-            PostQuitMessage(0);
-            break;
-        }
+	switch (message)
+	{
+	case WM_DESTROY:
+	{
+		for (const auto& [_, value] : hookingMap) {
+			// Try to unhook DLL (wil::unique_hhook::get()), do nothing if failed.
+			try {
+				UnhookWindowsHookEx(value);
+			}
+			catch (...) {}
+		}
+		PostQuitMessage(0);
+		break;
+	}
 	// Create a new case for our notification icon
 	case WM_USER + 1:
 		switch (LOWORD(lParam))
@@ -155,19 +155,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 			// Get the mouse location using WPARAM.
 			// Note: if the mouse is outside the notification icon rect, use the center point of the icon instead.
-            POINT pt = { GET_X_LPARAM(wParam), GET_Y_LPARAM(wParam) };
-			if (pt.x == -1 && pt.y == -1)
-			{
-				RECT rc;
-				// NotifyIconGetRect
-                NOTIFYICONIDENTIFIER id;
-                id.cbSize = sizeof(NOTIFYICONIDENTIFIER);
-                id.hWnd = hWnd;
-                id.uID = 1;
-				Shell_NotifyIconGetRect(&id, &rc);
-				pt.x = (rc.left + rc.right) / 2;
-				pt.y = (rc.top + rc.bottom) / 2;
-			}
+			POINT pt = { GET_X_LPARAM(wParam), GET_Y_LPARAM(wParam) };
 			HMENU hMenu = CreatePopupMenu();
 			AppendMenu(hMenu, MF_STRING, 1, L"Exit");
 			SetForegroundWindow(hWnd);
@@ -181,44 +169,44 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 		}
 		break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 void WinEventProc(
-    HWINEVENTHOOK hWinEventHook,
-    DWORD         event,
-    HWND          hwnd,
-    LONG          idObject,
-    LONG          idChild,
-    DWORD         idEventThread,
-    DWORD         dwmsEventTime)
+	HWINEVENTHOOK hWinEventHook,
+	DWORD         event,
+	HWND          hwnd,
+	LONG          idObject,
+	LONG          idChild,
+	DWORD         idEventThread,
+	DWORD         dwmsEventTime)
 {
-    HandleWindow(hwnd);
+	HandleWindow(hwnd);
 }
 
 void HandleWindow(HWND hwnd)
 {
-    DWORD processId;
-    DWORD tid = GetWindowThreadProcessId(hwnd, &processId);
-    HANDLE processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
-    TCHAR processName[MAX_PATH];
-    DWORD length = MAX_PATH;
-    QueryFullProcessImageNameW(processHandle, 0, processName, &length);
-    CloseHandle(processHandle);
+	DWORD processId;
+	DWORD tid = GetWindowThreadProcessId(hwnd, &processId);
+	HANDLE processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+	TCHAR processName[MAX_PATH];
+	DWORD length = MAX_PATH;
+	QueryFullProcessImageNameW(processHandle, 0, processName, &length);
+	CloseHandle(processHandle);
 
-    // Check if the process is the Windows Explorer
-    if (wcscmp(processName, L"C:\\Windows\\explorer.exe") == 0 && hookingMap.find(tid) == hookingMap.end())
-    {
-        HMODULE dll = LoadLibrary(L"Injector.dll");
-        if (!dll)
-            return;
-        auto procAddr = GetProcAddress(dll, "InjectExplorerHook");
-        auto method = reinterpret_cast<PFN_INJECT_EXPLORER_HOOK>(procAddr);
-        HHOOK hook = method(hwnd);
-        hookingMap.insert(std::pair<DWORD, HHOOK>(tid, hook));
-        FreeLibrary(dll);
-    }
+	// Check if the process is the Windows Explorer
+	if (wcscmp(processName, L"C:\\Windows\\explorer.exe") == 0 && hookingMap.find(tid) == hookingMap.end())
+	{
+		HMODULE dll = LoadLibrary(L"Injector.dll");
+		if (!dll)
+			return;
+		auto procAddr = GetProcAddress(dll, "InjectExplorerHook");
+		auto method = reinterpret_cast<PFN_INJECT_EXPLORER_HOOK>(procAddr);
+		HHOOK hook = method(hwnd);
+		hookingMap.insert(std::pair<DWORD, HHOOK>(tid, hook));
+		FreeLibrary(dll);
+	}
 }
